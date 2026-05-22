6c08ced3f481b81d507ffdc1d68279d05ef1a445
M-Anil13 <naniyadav1312@gmail.com>
2026-05-23 00:34:59 +0530

    fix: remove runtime export from proxy.js (not allowed in Next.js 16)

--- frontend/proxy.js
+++ frontend/proxy.js
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server';
 
-export const runtime = 'nodejs';
-
 export function proxy(request) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.rewrite(new URL('/home.html', request.url));
